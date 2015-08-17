147b2f4fd31a171a89afe03cb30cc0e2c067e263
李苦李 <80816909@qq.com>
2015-08-17 15:52:59 +0800

    Update library.php

--- library.php
+++ library.php
@@ -1,5 +1,44 @@
 <?php
 
+//array_column 等同于php5.5以后的array_column
+  function array_column($input, $columnKey, $indexKey = NULL)
+  {
+    $columnKeyIsNumber = (is_numeric($columnKey)) ? TRUE : FALSE;
+    $indexKeyIsNull = (is_null($indexKey)) ? TRUE : FALSE;
+    $indexKeyIsNumber = (is_numeric($indexKey)) ? TRUE : FALSE;
+    $result = array();
+ 
+    foreach ((array)$input AS $key => $row)
+    { 
+      if ($columnKeyIsNumber)
+      {
+        $tmp = array_slice($row, $columnKey, 1);
+        $tmp = (is_array($tmp) && !empty($tmp)) ? current($tmp) : NULL;
+      }
+      else
+      {
+        $tmp = isset($row[$columnKey]) ? $row[$columnKey] : NULL;
+      }
+      if ( ! $indexKeyIsNull)
+      {
+        if ($indexKeyIsNumber)
+        {
+          $key = array_slice($row, $indexKey, 1);
+          $key = (is_array($key) && ! empty($key)) ? current($key) : NULL;
+          $key = is_null($key) ? 0 : $key;
+        }
+        else
+        {
+          $key = isset($row[$indexKey]) ? $row[$indexKey] : 0;
+        }
+      }
+ 
+      $result[$key] = $tmp;
+    }
+ 
+    return $result;
+  }
+
 //
 $list = array_reduce($list, create_function('$v,$w', '$v[$w["id"]]=$w["name"];return $v;'));
 
